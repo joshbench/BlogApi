027f3e625247192e3e6e8ed432401db790ccc0b9
joshbench <joshtbench@gmail.com>
2018-01-28 20:16:50 -0800

    Added comments

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ import os, json
 
 app = Flask(__name__)
 
+## Accepts 'body' and 'title' elements in json, and insterts
+## them into the db
 @app.route('/post', methods=['POST'])
 def create_post():
     post_data = request.get_json()
-    print (post_data)
+    # Need to have this write to the db instead of printing
+    print(post_data)
     return 'json posted'
 
+## Gets all posts from the db, no parameters
 @app.route('/posts', methods=['GET'])
 def get_posts():
+    # placeholder json while I configure the db
     message = {
         'uid': '11289364',
         'title': 'title',
